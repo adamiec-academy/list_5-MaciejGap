19f5a15897b7f2ff66f7b788fae3361192386df9
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-12-31 16:34:26 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,6 +1,8 @@
 def unique(data):
     result = []
+    set_result = set()
     for element in data:
-        if element not in result:
+        if element not in set_result:
             result.append(element)
+            set_result.add(element)
     return result
