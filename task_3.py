4beec1b8908a555043de4d60cf54116f476922ea
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2023-01-03 14:43:03 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,17 @@
-# Remove this comment to confirm that this task is done
+from turtle import *
+
+def painting():
+    data = []
+    for line in open("dots.txt", encoding="utf-8"):
+        x = line.strip().split()
+        p = (int(x[0]), int(x[1]))
+        data.append(p)
+    penup()
+    x, y = data[0]
+    goto(x // 2 - 150, -y // 2 + 150)
+    pendown()
+    for dot in data:
+        x, y = dot
+        goto(x//2 - 150, -y//2 + 150)
+    exitonclick()
+
