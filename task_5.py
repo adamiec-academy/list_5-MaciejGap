5fd43d86864bb1f8f99a79085da945aa5476ccba
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2023-01-04 16:24:25 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,2 +1,7 @@
 def tower_of_hanoi(n, source, dest, aux):
-    return
+    if n == 0:
+        return
+    tower_of_hanoi(n - 1, source, aux, dest)
+    print("Move disk", n, "from", source, "to", dest)
+    tower_of_hanoi(n - 1, aux, dest, source)
+    
